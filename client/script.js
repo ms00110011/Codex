f099d1c88e1f4a1c12fe8205152ca4a46915d606
ms00110011 <ms00110011@gmail.com>
2023-01-11 12:42:36 +0530

    connected deployed server

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetching data from the server
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codeasd.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
